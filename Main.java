78066579d71f0b15e04d9b849b08b208eca92fc4
Zack Nelson <eagletrail342@gmail.com>
2026-09-15 23:22:46 -0400

    Update Main.java
    added author tags and param tag that might need reworking as file expands.

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
+/** Java Docs experimentation Class
+ * 
+ * @author Zackary Nelson
+ * @author Bradly Patton
+ * @author Ulises Royal
+ * @author Peyton Slusser
+ * @param text file
+ */
+
 public class Main{
   public static void main(String[] args){
-
     
   }
 }
