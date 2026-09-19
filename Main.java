a2b81eb4ebc206394376ad6acaf6d04596769765
pslusser1 <g7t844b2g9@privaterelay.appleid.com>
2026-09-19 14:24:09 -0400

    Added comments

--- Main.java
+++ Main.java
@@ -16,26 +16,29 @@ import java.util.Scanner;
 public class Main{
   public static void main(String[] args){
 
+    //asks what word to search for
     Scanner scanner = new Scanner(System.in);
     System.out.println("What word would you like to find?");
     String ourWord = scanner.next();
-
-    //checking for letter
-    int location = 0;
-    char letter = ourWord.charAt(location);
 
     //asking the user the name of the file they'd like to read.
     System.out.println("What file would you like to read:");
     String findFile = scanner.next();
     scanner.close();
 
-    //testing file reading
+    //looking at char in search word
+    int location = 0;
+    char letter = ourWord.charAt(location);
 
+    //opens file
     File file = new File(findFile);
+    
+    //reads data in file and creates a string for a line in the file
     try (Scanner myReader = new Scanner(file)) {
       while (myReader.hasNextLine()) {
         String line = myReader.nextLine();
-        
+
+        //searches a line in the file for a letter from search word
         for (int i = 0; i < line.length(); i++) {
           if (line.charAt(i) == letter) {
             System.out.println("Found " + letter + " at index " + i);
