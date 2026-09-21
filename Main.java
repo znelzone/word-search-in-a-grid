2c0a8c7e5f4da25fd315d88847022c4dcb3dd6ad
pslusser1 <g7t844b2g9@privaterelay.appleid.com>
2026-09-21 17:03:18 -0400

    create a 2d array from a file

--- Main.java
+++ Main.java
@@ -13,9 +13,11 @@
  * @param text file
  */
 
+import java.util.List;
+import java.util.ArrayList;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class Main{
   public static void main(String[] args){
@@ -23,7 +25,7 @@ public class Main{
     //asks what word to search for in the file
     Scanner scanner = new Scanner(System.in);
     System.out.println("What word would you like to find?");
-    String ourWord = scanner.nextLine();
+    String searchWord = scanner.nextLine();
 
     //asking the user for the name of the file to search in
     System.out.println("What file would you like to read:");
@@ -35,29 +37,47 @@ public class Main{
 
     //looking at char in search word
     int location = 0;
-    char letter = ourWord.charAt(location);
+    char letter = searchWord.charAt(location);
 
-    //reads data in file and creates a string for a line in the file
-    try (Scanner myReader = new Scanner(file)) {
-      while (myReader.hasNextLine()) {
-        String line = myReader.nextLine();
+    try {
+      //Opens the file
+      Scanner fileScanner = new Scanner(file);
+      List<char[]> rows = new ArrayList<>();
 
-        //searches a line in the file for a letter from search word
-        for (int i = 0; i < line.length(); i++) {
-          if (line.charAt(i) == letter) {
-            System.out.println("Found " + letter + " at index " + i);
-          } else {
-            System.out.println("Not found");
-          }
+      //reads each line of file
+      while (fileScanner.hasNextLine()) {
+        String line = fileScanner.nextLine();
+
+        //splits string into chars
+        String[] parts = line.split(" ");
+        char[] row = new char[parts.length];
+
+        //converts string into chars
+        for (int i = 0; i < parts.length; i++) {
+          row[i] = parts[i].charAt(0);
         }
+        rows.add(row);
       }
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred, file not found.");
-      e.printStackTrace();
+
+      fileScanner.close();
+
+      //converts into 2d char array
+      char[][] grid = rows.toArray(new char[0][]);
+
+    }catch (FileNotFoundException e) {
+      System.out.println("File not found: " + findFile);
     }
 
-    
-  }
+
+
+
+
+
+
+
+
+
+
 
     /**Copys a String into a character array placing a space
      * between each character in the array.
@@ -66,37 +86,37 @@ public class Main{
      * @throws IllegalArgumentException if text is empty
      * @since version 0.1.1 
      */
-    public static char[] spacedChararray(String text){
+    // public static char[] spacedChararray(String text){
         
-        if(text.length() == 0){
-          //if the string is empty throws an exception
-          throw new IllegalArgumentException("String cannont be empty.");
-          //maybe should change this to "please enter a word"
+    //     if(text.length() == 0){
+    //       //if the string is empty throws an exception
+    //       throw new IllegalArgumentException("String cannont be empty.");
+    //       //maybe should change this to "please enter a word"
         
-        } else if (text.length()==1){
-          //if string has one character makes an array with one character
+    //     } else if (text.length()==1){
+    //       //if string has one character makes an array with one character
           
-          char[] extendedArray = new char[1];
-          extendedArray[0] = text.charAt(0);
-          return extendedArray;
+    //       char[] extendedArray = new char[1];
+    //       extendedArray[0] = text.charAt(0);
+    //       return extendedArray;
         
-        } else (text.length()>1){
-          //converts the string into an array of chars with
-          //spaces between each char.
+    //     } else (text.length()>1){
+    //       //converts the string into an array of chars with
+    //       //spaces between each char.
           
-          char[] extendedArray = new char[(text.length()*2)-1];
-          for(int arrayloc = 0, strloc = 0;strloc < text.length();arrayloc+=2, strloc++){
+    //       char[] extendedArray = new char[(text.length()*2)-1];
+    //       for(int arrayloc = 0, strloc = 0;strloc < text.length();arrayloc+=2, strloc++){
               
-              extendedArray[arrayloc] = text.charAt(strloc);
+    //           extendedArray[arrayloc] = text.charAt(strloc);
               
-              if(arrayloc < extendedArray.length-1){
-              extendedArray[arrayloc+1] = ' ';
-              }
-          }
-          return extendedArray;
-        }
+    //           if(arrayloc < extendedArray.length-1){
+    //           extendedArray[arrayloc+1] = ' ';
+    //           }
+    //       }
+    //       return extendedArray;
+    //     }
           
-    }
+  }
 
 
 }
