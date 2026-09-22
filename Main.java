395661671c43058f0ffac414fa25bd5cf981b57d
Zack Nelson <eagletrail342@gmail.com>
2026-09-22 03:15:33 -0400

    fully realized horizontalSearch

--- Main.java
+++ Main.java
@@ -22,7 +22,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
   public static void main(String[] args) {
-    
+
     boolean wordFound = false;
 
     // asks what word to search for in the file
@@ -35,19 +35,19 @@ public class Main {
     String gridFile = scanner.nextLine();
     scanner.close();
 
-    //creates our 2D array that we will be converting our file into
+    // creates our 2D array that we will be converting our file into
     char[][] ourGrid = null;
 
     // tries to read the specified file and convert its content into a 2D char array
-    //while eliminating the spaces between characters from the original file for 
-    //easier searching for the searchWord
+    // while eliminating the spaces between characters from the original file for
+    // easier searching for the searchWord
     try {
       // places the desired file into our file variable wordBoard
       File wordBoard = new File(gridFile);
       // create a filescanner ie open our file
       Scanner fileScanner = new Scanner(wordBoard);
 
-      //makes the char list and fills it with the rows of words from our file.
+      // makes the char list and fills it with the rows of words from our file.
       List<char[]> rowList = new ArrayList<char[]>();
       while (fileScanner.hasNextLine()) {
         String wordBoardLine = fileScanner.nextLine();
@@ -63,26 +63,24 @@ public class Main {
       System.out.println("File not found: " + gridFile);
     }
 
-
-    //this set of if statements utilizes a set of three methods to determine
-    //if the word we're looking for is infact in the text file
-    if(horizontalSearch(ourGrid,searchWord)){
+    // this set of if statements utilizes a set of three methods to determine
+    // if the word we're looking for is infact in the text file
+    if (horizontalSearch(ourGrid, searchWord)) {
       wordFound = true;
-    } else if (verticalSearch(ourGrid,searchWord)){
+    } else if (verticalSearch(ourGrid, searchWord)) {
       wordFound = true;
-    } else if (diagonalSearch(ourGrid,searchWord)){ 
+    } else if (diagonalSearch(ourGrid, searchWord)) {
       wordFound = true;
     }
 
-
-    //final output statement's
-    if(wordFound){
+    // final output statement's
+    if (wordFound) {
       System.out.println("Your Word Found!");
     } else {
       System.out.println("Your Word Not Found");
     }
 
-    //end of main method
+    // end of main method
   }
 
   /**
@@ -117,66 +115,84 @@ public class Main {
       return nonSpaced;
     }
 
-    //end of convertSpacedStringToArray method
+    // end of convertSpacedStringToArray method
   }
 
-  /** Searches diagonally through a 2D array for a string.
+  /**
+   * Searches diagonally through a 2D array for a string.
+   * 
    * @param twoDArray a 2D array to be searched through diagonally
-   * @param text the String we're looking for in the 2D array
+   * @param text      the String we're looking for in the 2D array
    * @return true or false based on if the string was found
    * @since version 0.1.2
-  */
-  public static boolean diagonalSearch(char[][] twoDArray, String text){
+   */
+  public static boolean diagonalSearch(char[][] twoDArray, String text) {
 
     return false;
-    
-    //end of diagonalSearch method
+
+    // end of diagonalSearch method
   }
 
-  /** Searches horizontally through a 2D array for a string
+  /**
+   * Searches horizontally through a 2D array for a string
+   * 
    * @param twoDArray a 2D array to be searched through horizontally
-   * @param text the String we're looking for in the 2D array
+   * @param text      the String we're looking for in the 2D array
    * @return true or false based on if the string was found
    * @since version 0.1.2
-  */
-  public static boolean horizontalSearch(char[][] twoDArray, String text){
+   */
+  public static boolean horizontalSearch(char[][] twoDArray, String text) {
 
-    //horizontal search
-    for(int row=0;row<twoDArray.length;row++){
-      int col = 0;
-      int wordIndex = 0;
-        while(col <= twoDArray[row].length-text.length()){
-          if(twoDArray[row][col]==text.charAt(wordIndex)){
+    // horizontal search
+    // goes through each row
+    for (int row = 0; row < twoDArray.length; row++) {
 
+      // goes through the columns without falling out of the array
+      for (int col = 0; col <= twoDArray[row].length - text.length(); col++) {
+        boolean match = false;
 
+        //compares each index of text with the current starting point being whichever
+        //column the column loop is currently pointing at
+        for (int wordIndex = 0; wordIndex < text.length(); wordIndex++) {
+          
+          if (twoDArray[row][col + wordIndex] == text.charAt(wordIndex)) {
+            match = true;
+          } else {
+            // if at any point the word doesn't match with whats in the row
+            // break out of loop after recording a false value. this will
+            // cause loops to move to the next col
+            match = false;
+            break;
           }
-          col++;
         }
-        
+        //having made it out of the word checking loop, if every char was a match
+        //then this if statement should trigger and cause the method to return true
+        //it has found a matching word.
+        if (match) {
+          return true;
+        }
+
+      }
     }
-
-
-
-
-
-
     return false;
-    
-    //end of horizontalSearch method
+
+    // end of horizontalSearch method
   }
 
-  /** Searches vertically through a 2D array for a string
+  /**
+   * Searches vertically through a 2D array for a string
+   * 
    * @param twoDArray a 2D array to be searched through vertically
-   * @param text the String we're looking for in the 2D array
+   * @param text      the String we're looking for in the 2D array
    * @return true or false based on if the string was found
    * @since version 0.1.2
-  */
-  public static boolean verticalSearch(char[][] twoDArray, String text){
-    
+   */
+  public static boolean verticalSearch(char[][] twoDArray, String text) {
+
     return false;
 
-    //end if verticalSearch method
+    // end if verticalSearch method
   }
 
-  //end of Main class
+  // end of Main class
 }
