37f354e13b23fe8a3aad238cfbe0bc43357b93cc
Zack Nelson <eagletrail342@gmail.com>
2026-09-21 23:19:32 -0400

    Implemented convertSpacedStringToArray into peytons file to 2d array code.

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 /**
  * Reads a text file and searches for a word within the file.
  * If the word is found, will return found,
@@ -19,112 +20,81 @@ import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 
-public class Main{
-  public static void main(String[] args){
-    
-    //asks what word to search for in the file
+public class Main {
+  public static void main(String[] args) {
+
+
+    // asks what word to search for in the file
     Scanner scanner = new Scanner(System.in);
     System.out.println("What word would you like to find?");
     String searchWord = scanner.nextLine();
 
-    //asking the user for the name of the file to search in
+    // asking the user for the name of the file to search in
     System.out.println("What file would you like to read:");
-    String findFile = scanner.next();
+    String gridFile = scanner.nextLine();
     scanner.close();
-    //opens file
-    File file = new File(findFile);
 
 
-
-
-
-    //loop to interate through 2d array
-//    for(int row = 0, col = 0; col < searchArray; row++){
-//        while(col < searchArray.lenght){
-//                
-//
-//
-//         col++;
-//      }
-//  }
-
-
-    int location = 0;
-    char letter = searchWord.charAt(location);
-
+    // tries to read the specified file and convert its content into a 2D char array
     try {
-      //Opens the file
-      Scanner fileScanner = new Scanner(file);
-      List<char[]> rows = new ArrayList<>();
+      // places the desired file into our file variable wordBoard
+      File wordBoard = new File(gridFile);
+      // create a filescanner ie open our file
+      Scanner fileScanner = new Scanner(wordBoard);
 
-      //reads each line of file
+      //makes the char list and fills it with the rows of words from our file.
+      List<char[]> rowList = new ArrayList<char[]>();
       while (fileScanner.hasNextLine()) {
-        String line = fileScanner.nextLine();
-
-        //splits string into chars
-        String[] parts = line.split(" ");
-        char[] row = new char[parts.length];
-
-        //converts string into chars
-        for (int i = 0; i < parts.length; i++) {
-          row[i] = parts[i].charAt(0);
-        }
-        rows.add(row);
+        String wordBoardLine = fileScanner.nextLine();
+        char[] row = convertSpacedStringToArray(wordBoardLine);
+        rowList.add(row);
       }
-
       fileScanner.close();
 
-      //converts into 2d char array
-      char[][] grid = rows.toArray(new char[0][]);
+      // converts into 2d char array
+      char[][] ourGrid = rowList.toArray(new char[0][]);
 
-    }catch (FileNotFoundException e) {
-      System.out.println("File not found: " + findFile);
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found: " + gridFile);
     }
 
 
 
 
+    
+  }
 
+  /**
+   * Converts a String where each char has a space
+   * in between into a Char[] that eliminates the spaces
+   * between chars.
+   * 
+   * @param text the string to be converted
+   * @return a char array without spaces between each char
+   * @throws IllegalArgumentException if text is empty
+   * @since version 0.1.1
+   */
+  public static char[] convertSpacedStringToArray(String text) {
 
+    if (text.length() == 0) {
+      // if the string is empty throws an exception
+      throw new IllegalArgumentException("String cannot be empty.");
 
+    } else if (text.length() == 1) {
+      // if string has one character makes an array with one character
+      char[] nonSpaced = text.toCharArray();
+      return nonSpaced;
 
+    } else {
+      // converts the string into an array of characters without
+      // spaces between the characters.
+      char[] nonSpaced = new char[(text.length() + 1) / 2];
 
-
-
-
-    /**Copys a String into a character array placing a space
-     * between each character in the array.
-     * @param text the string to be converted
-     * @return a char array with spaces between each original character
-     * @throws IllegalArgumentException if text is empty
-     * @since version 0.1.1 
-     */
-    public static char[] convertSpacedstringtoarray(String text){
-
-      if(text.length() == 0){
-          //if the string is empty throws an exception
-          throw new IllegalArgumentException("String cannont be empty.");
-          //maybe should change this to "please enter a word"
+      for (int arrayind = 0, textind = 0; textind < text.length(); arrayind++, textind += 2) {
+        nonSpaced[arrayind] = text.charAt(textind);
       }
-
-        
-    //     } else if (text.length()==1){
-    //       //if string has one character makes an array with one character
-          
-    //       char[] extendedArray = new char[1];
-    //       extendedArray[0] = text.charAt(0);
-    //       return extendedArray;
-        
-    //     } else (text.length()>1){
-    //       //converts the string into an array of chars with
-    //       //spaces between each char.
-          
-    //       char[] extendedArray = new char[(text.length()*2)-1];
-    //       for(int arrayloc = 0, strloc = 0;strloc < text.length();arrayloc+=2, strloc++){
-              
-    //           extendedArray[arrayloc] = text.charAt(strloc);
-          
+      return nonSpaced;
     }
-
+  }
 
 }
